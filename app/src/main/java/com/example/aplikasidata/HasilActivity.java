d6cff8567d4987a012085ac7906385b4a2712dce
vionnymonica <vionymonica12@gmail.com>
2023-03-22 00:22:33 +0700

    +monica

--- app/src/main/java/com/example/aplikasidata/HasilActivity.java
+++ app/src/main/java/com/example/aplikasidata/HasilActivity.java
@@ -13,6 +13,7 @@ public class HasilActivity extends AppCompatActivity {
     private TextView txtEmail,txtPassword;
     private Button btnReset;
 //viony
+    //monica
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
